5007f215ef9ff09e31d8ba8f20b8d6a3fcf50ae3
dimuthuY <it22122964@my.sliit.lk>
2024-05-11 21:21:12 +0530

    2 nd commit

---
+++ app/src/main/java/com/example/dimuthu/NoteAdapter.kt
@@ -0,0 +1,7 @@
+package com.example.dimuthu
+
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+
+class NoteAdapter(private var  notes: List<Note>, context:Context):RecyclerView.Adapter<NotesAdapter.> {
+}

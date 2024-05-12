32888d8b3cff951591bb093e8a0e3a5f0251897b
dimuthuY <it22122964@my.sliit.lk>
2024-05-12 12:31:56 +0530

    5th

--- app/src/main/java/com/example/dimuthu/NoteAdapter.kt
+++ app/src/main/java/com/example/dimuthu/NoteAdapter.kt
@@ -1,30 +1,40 @@
 package com.example.dimuthu
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class NoteAdapter(private var  notes: List<Note>, context:Context):RecyclerView.Adapter<NoteAdapter.NoteVieaHolder> {
+class NoteAdapter(private var  notes: List<Note>, context:Context):RecyclerView.Adapter<NoteAdapter.NoteViewHolder>(){
 
     class NoteViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
         val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
-        val contetTextView: TextView = itemView.findViewById(R.id)
+        val contetTextView: TextView = itemView.findViewById(R.id.contentTextView)
+        val updateButton: ImageView = itemView.findViewById(R.id.updateSaveButton)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteAdapter.NoteVieaHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteAdapter.NoteViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.note_item,parent,false)
         return NoteViewHolder(view)
     }
 
-    override fun getItemCount(): Int = note.size
+    override fun getItemCount(): Int = notes.size
 
-    override fun onBindViewHolder(holder: NoteAdapter.NoteVieaHolder, position: Int) {
+    override fun onBindViewHolder(holder: NoteAdapter.NoteViewHolder, position: Int) {
          val note = notes[position]
         holder.titleTextView.text = note.title
-        holder.contentTextView.text = note.content
+        holder.contetTextView.text = note.content
+
+        holder.updateButton.setOnClickListener{
+            val intent = Intent(holder.itemView.context,UpdateActivity::class.java).apply {
+                putExtra("note_id",note.id)
+            }
+             holder.itemView.context.startActivity(intent)
+        }
     }
 
 
--- app/src/main/java/com/example/dimuthu/NotesDatabaseHelper.kt
+++ app/src/main/java/com/example/dimuthu/NotesDatabaseHelper.kt
@@ -57,4 +57,13 @@ class NotesDatabaseHelper(context: Context):SQLiteOpenHelper(context,DATABASE_NA
         db.close()
         return notesList
         }
+
+    fun  updateNote(note:Note){
+        val db = writableDatabase
+        val values = ContentValues().apply {
+            put(COLUMN_TITLE,note.title)
+            put(COLUMN_CONTENT,note.content)
+        }
+        val whereClause = "$"
     }
+}
